af3a92157f24018e0a8dad4c8fe1b534da9ae1ef
John Vundi <johnsonvundy2@gmail.com>
2023-07-13 09:54:10 +0300

    continuation

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -1,6 +1,13 @@
 import  './leftbar.css'
 import RssFeedIcon from '@mui/icons-material/RssFeed';
-
+import ChatIcon from '@mui/icons-material/Chat';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import GroupsIcon from '@mui/icons-material/Groups';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import EventIcon from '@mui/icons-material/Event';
+import SchoolIcon from '@mui/icons-material/School';
 function Leftbar() {
   return (
     <div className='leftbar'>
@@ -11,24 +18,36 @@ function Leftbar() {
                 <div className="sidebarListItemText">Feed</div>
                 </li>
                 <li className="leftbarListItem">
-                <RssFeedIcon className='sidebarIcon' />
-                <div className="sidebarListItemText">Feed</div>
+                <ChatIcon className='sidebarIcon' />
+                <div className="sidebarListItemText">Chat</div>
                 </li>
                 <li className="leftbarListItem">
-                <RssFeedIcon className='sidebarIcon' />
-                <div className="sidebarListItemText">Feed</div>
+                <PlayCircleIcon  className='sidebarIcon' />
+                <div className="sidebarListItemText">Videos</div>
                 </li>
                 <li className="leftbarListItem">
-                <RssFeedIcon className='sidebarIcon' />
-                <div className="sidebarListItemText">Feed</div>
+                <GroupsIcon className='sidebarIcon' />
+                <div className="sidebarListItemText">Groups</div>
                 </li>
                 <li className="leftbarListItem">
-                <RssFeedIcon className='sidebarIcon' />
-                <div className="sidebarListItemText">Feed</div>
+                <BookmarkIcon  className='sidebarIcon' />
+                <div className="sidebarListItemText">Bookmarks</div>
                 </li>
                 <li className="leftbarListItem">
-                <RssFeedIcon className='sidebarIcon' />
-                <div className="sidebarListItemText">Feed</div>
+                <HelpOutlineIcon className='sidebarIcon' />
+                <div className="sidebarListItemText">Questions</div>
+                </li>
+                <li className="leftbarListItem">
+                <WorkOutlineIcon  className='sidebarIcon' />
+                <div className="sidebarListItemText">Jobs</div>
+                </li>
+                <li className="leftbarListItem">
+                <EventIcon  className='sidebarIcon' />
+                <div className="sidebarListItemText">Events</div>
+                </li>
+                <li className="leftbarListItem">
+                <SchoolIcon  className='sidebarIcon' />
+                <div className="sidebarListItemText">Courses</div>
                 </li>
             </ul>
             <button className='sidebarButton'>Show more</button>
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,13 +1,38 @@
 import  './share.css'
+import LabelIcon from '@mui/icons-material/Label';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
+import PermMediaIcon from '@mui/icons-material/PermMedia';
 function Share() {
   return (
     <div className='share'>
        <div className="shareWrapper">
         <div className="shareTop">
-            <img src="" alt="" />
+            <img className='shareProfileImg' src="/assets/persons/Profile.jpeg" alt="" />
+            <input placeholder="what's in your mind" className='shareInput' />
         </div>
-       <div className="shareBottom"></div>
+        <hr className='shareHr'/>
+       <div className="shareBottom">
+         <div className="shareOptions">
+            <div className="shareOption">
+                <PermMediaIcon className='shareIcon'/>
+                <span className='shareOptionText'>Photo or Video</span>
+            </div>
+            <div className="shareOption">
+                <LabelIcon className='shareIcon'/>
+                <span className='shareOptionText'>Photo or Video</span>
+            </div>
+            <div className="shareOption">
+                <LocationOnIcon className='shareIcon'/>
+                <span className='shareOptionText'>Photo or Video</span>
+            </div>
+            <div className="shareOption">
+                <EmojiEmotionsIcon className='shareIcon'/>
+                <span className='shareOptionText'>Photo or Video</span>
+            </div>
+         </div>
+       </div>
        </div>
     </div>
   )
