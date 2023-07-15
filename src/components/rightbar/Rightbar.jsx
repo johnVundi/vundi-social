b1cb9fa09667fab38df9c7dd70cef3da4a028faa
John Vundi <johnsonvundy2@gmail.com>
2023-07-15 10:23:18 +0300

    complete post component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,17 @@ import  './rightbar.css'
 
 function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>
+    <div className="rightbarWrapper">
+      <div className="birthdayContainer">
+        <img src="/assets/persons/person3.png" alt="" className="birthdayImg" />
+        <span className="birthdayText">
+          {""}
+          <b>Otis</b> and <b>3 others</b> have a birthday today
+        </span>
+      </div>
+    </div>
+    </div>
   )
 }
 
